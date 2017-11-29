cfca1070c475eccf73fc81a731d8971675ba1168
Souha Guissouma <sguissouma@emi.u-bordeaux1.fr>
2017-11-29 10:01:15 +0100

    new files

---
+++ src/model/Graph.java
@@ -0,0 +1,83 @@
+package model;
+<<<<<<< HEAD
+import java.awt.List;
+=======
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+>>>>>>> 819deb88b9fabb4b07a7894775f9d24c0e5b0c65
+import java.util.ArrayList;
+
+import org.jgrapht.graph.SimpleGraph;
+
+@SuppressWarnings("serial")
+public class Graph extends SimpleGraph<Vertex,Edge>{
+
+	public Graph() {
+		super(Edge.class);
+	}
+	
+	public boolean addVertex(Vertex v) {
+		boolean ret = super.addVertex(v);
+		return ret;
+	}
+	
+	public boolean contains(Vertex vertex) {
+		ArrayList<Vertex> v = new ArrayList<Vertex>(this.vertexSet());
+		//System.out.println("TEST :" + v);
+		for(Vertex v0 : v) {
+			if(v0.equals(vertex))
+				return true;
+		}
+		return false;
+	}
+<<<<<<< HEAD
+=======
+	
+	/*
+	 * 	graph MyGraph{
+	 * 	  V_0_0 [label="0,0"];
+	 *    etc
+	 *    
+	 *    V_0_0 -- V_0_1 [];
+	 *    
+	 *  }
+	 *  
+	 *  dot -Tpng -o mongraphe.png mongraphe.dot
+	 */
+	
+	public void toDot(String fileName) throws IOException {
+		File f = new File(fileName);
+		FileWriter w = new FileWriter(f);
+		
+		w.write("graph MyGraph{\n");
+		String buf = new String("");
+		
+		for (Vertex v : this.vertexSet()) {
+			buf = "V_" + v.getX() + "_" + v.getY() + " [label=\"" + v.getX()+ ","+v.getY()+"\"];\n";
+			w.write(buf);
+ 		}
+		
+		Vertex v1, v2;
+		for(Edge e : this.edgeSet()) {
+			v1 = this.getEdgeSource(e);
+			v2 = this.getEdgeTarget(e);
+			
+			buf = "V_" + v1.getX() + "_" + v1.getY() + " -- " + "V_" + v2.getX() + "_" + v2.getY() + ";\n";
+			
+//			System.out.println(this.getEdgeSource(e).toString());
+//			System.out.println("|");
+//			System.out.println(this.getEdgeTarget(e).toString());
+			
+			w.write(buf);
+		}
+		
+		w.write("}");;
+		
+		w.close();
+	}
+>>>>>>> 819deb88b9fabb4b07a7894775f9d24c0e5b0c65
+
+	
+}
---
+++ src/model/Labyrinth.java
@@ -0,0 +1,149 @@
+package model;
+
+<<<<<<< HEAD
+=======
+import java.awt.image.DirectColorModel;
+import java.io.IOException;
+>>>>>>> 819deb88b9fabb4b07a7894775f9d24c0e5b0c65
+import java.util.Random;
+
+public class Labyrinth {
+	private Graph graph;
+	private Random rand;
+	public static int size = 4;
+	
+	public static final int TOP_BORDER = 0;
+	public static final int RIGHT_BORDER = size;
+	public static final int LEFT_BORDER = 0;
+	public static final int BOTTOM_BORDER = size;
+
+	private int width;
+	private int height;
+	
+	public Labyrinth(Vertex v) {
+		width = size;
+		height = size;
+		graph = new Graph();
+		graph.addVertex(v);
+		rand = new Random();
+		
+		if(graph.contains(new Vertex(0,0)))
+			System.out.println("OK");
+		
+		buildRandomPath(v);
+	}
+
+	
+	public int getWidth() {
+		return width;
+	}
+
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
+	private void buildRandomPath(Vertex vertex) {
+
+		//initialisation des directions
+		Directions directions[] = Directions.values();
+
+		//pour chacune de ces directions, on avance en profondeur d'abord
+		for(int i=0;i<4; i++) {
+			Directions dir = directions[i];
+			//TODO fonction en prédicat à implémenter si on garde l'exemple du prof
+			if ((vertex.inBorders(dir,this)) & (graph.contains(vertex))) {
+				int x = vertex.getX();
+				int y = vertex.getY();
+				int xt = 0;
+				int yt = 0;
+				switch(dir) {
+				case NORTH : xt= x;
+				yt= y-1;
+				break;
+				case SOUTH : xt=x;
+				yt= y+1;
+				break;
+				case EAST : xt =x+1;
+				yt=y;
+				break;
+				case WEST : xt=x-1;
+				yt=y;
+				break;				
+				}
+				Vertex next = new Vertex(xt, yt);
+				graph.addVertex(next);
+				graph.addEdge(vertex, next);
+				buildRandomPath(next); 
+				//TODO : ça marche pas !
+			} 
+		}
+	}
+
+	public void printGraph() {
+		System.out.println(graph.toString());
+		try {
+			graph.toDot("Laby.dot");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+//	public void buildLabyrinth(Vertex v) {
+//		int c;
+//		for(int i=0; i<4; i++) {
+//			c = rand.nextInt(4);
+//			//System.out.println("c = "+ c);
+//			Vertex v1 = new Vertex(v.getX(),v.getY());
+//			if(c == Constants.D_NORTH) {
+//				v1.setY(v1.getY()-1);
+//				if(!graph.containsVertex(v1) && v1.getY()>=Constants.TOP_BORDER) {
+//					System.out.println("NORTH");
+//					graph.addVertex(v1);
+//					graph.addEdge(v, v1);
+//				}
+//			}
+//			else if(c == Constants.D_EAST) {
+//				v1.setX(v1.getX()+1);
+//				if(!graph.containsVertex(v1) && v1.getX()<Constants.RIGHT_BORDER) {
+//					System.out.println("EAST");
+//					graph.addVertex(v1);
+//					graph.addEdge(v, v1);
+//				}
+//			}
+//			else if(c == Constants.D_SOUTH) {
+//				v1.setY(v1.getY()+1);
+//				if(!graph.containsVertex(v1) && v1.getY()<Constants.BOTTOM_BORDER) {
+//					System.out.println("SOUTH");
+//					graph.addVertex(v1);
+//					graph.addEdge(v, v1);
+//				}
+//			}
+//			else if(c == Constants.D_WEST) {
+//				v1.setX(v1.getX()-1);
+//				if(!graph.containsVertex(v1) && v1.getX()>=Constants.LEFT_BORDER) {
+//					System.out.println("WEST");
+//					graph.addVertex(v1);
+//					graph.addEdge(v, v1);
+//				}
+//			}
+//			printGraph();
+//			buildLabyrinth(v1);
+//		}
+//	}
+	
+
+
+}
