267a86404e513e5ca6e1a6d30a3a906b4abba16d
cvillavicencio01 <cvillavicencio01@gmail.com>
2017-12-05 21:07:13 +0100

    add position coordinates class

---
+++ src/model/Coord2D.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Carlos Villavicencio
+ *
+ */
+public class Coord2D {
+	public int x;
+	public int y;
+
+	public Coord2D(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return this.y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
