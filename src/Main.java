53f4c4b4198c6000483ae04220e31d6b6d9d5846
VastareMetal <31819272+VastareMetal@users.noreply.github.com>
2017-12-06 09:21:31 +0100

    Plus besoin de l'interface swing
    On lance directement la fenêtre du Labyrinthe quand on démarre le programme dans main.

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,12 @@ import controller.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main{ //peut-être extends Application, si on garde la version en commentaire...
-
-	//	public static void main(String[] args) {
-	//		launch();
-	//		
-	//	}
-	//
-	//	@Override
-	//	public void start(Stage primaryStage) throws Exception {
-	//		Controller.makeInstance();
-	//		Controller.start(primaryStage);
-	//	}
-	//	
-	//	public void stop() {
-	//		System.exit(0);
-	//	}
-
-	private GestionnaireGraphique gestion;
+public class Main{
 
 	public static void main(String[] args) {
 
-
-		/* Mise en place du gestionnaire graphique et lancement */
-		GestionnaireGraphique gestion = new GestionnaireGraphique();
-		gestion.run(); 
+		GameWindow gw = new GameWindow();
+		gw.open();
 	}
 
 
