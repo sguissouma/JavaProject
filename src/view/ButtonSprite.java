30c4763e9391e42b74b3f8eda0e55e9a952ef15a
Carlos Villavicencio <cvillavicencio01@gmail.com>
2017-12-20 18:00:14 +0100

    button sprite

---
+++ src/view/ButtonSprite.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.Button;
+
+public class ButtonSprite extends Sprite {
+	
+	private Button button;
+	
+	public void setButton(Button button) {
+		this.button = button;
+	}
+	
+	
+}
