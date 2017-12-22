d94525414cd20325cebd9b5128b995747831e659
Carlos Villavicencio <cvillavicencio01@gmail.com>
2017-12-22 04:34:43 +0100

    javadoc

--- src/helpers/Coord2D.java
+++ src/helpers/Coord2D.java
@@ -4,30 +4,52 @@
 package helpers;
 
 /**
+ * Class that handles a simple 2D coordinate system.
+ * 
  * @author Carlos Villavicencio
- *
  */
 public class Coord2D {
 	public int x;
 	public int y;
 
+	/**
+	 * Class constructor
+	 * @param x X coordinate for position
+	 * @param y Y coordinate for position
+	 */
 	public Coord2D(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Gets X coordinate for position.
+	 * @return X coordinate
+	 */
 	public int getX() {
 		return this.x;
 	}
 
+	/**
+	 * Set X coordinate for position.
+	 * @param X coordinate
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Gets Y coordinate for position.
+	 * @return Y coordinate
+	 */
 	public int getY() {
 		return this.y;
 	}
 
+	/**
+	 * Set Y coordinate for position.
+	 * @param Y coordinate
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
